d86ba80a3693d53de45529192ae5ff0fcb726b4a
muhammedadil01 <muhammedadil932@gmail.com>
2023-07-05 11:28:21 +0530

    task commit

---
+++ OddOrEven.js
@@ -0,0 +1,17 @@
+
+function CheckEvenOrOdd (val){
+
+    if(val % 2===0){
+      return  console.log("The number is even : "+val);
+
+    }else{
+      return   console.log("The number is odd : "+val);
+    }
+
+}
+
+var val = 25
+
+let result = CheckEvenOrOdd(val)
+
+console.log(result);
---
+++ checkpalindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(str) {
+    
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+   
+    var reversed = str.split('').reverse().join('');
+  
+
+    return str === reversed;
+  }
+  
+
+  var string1 = "racecar";
+  console.log(isPalindrome(string1));  
+  
+  var string2 = "hello";
+  console.log(isPalindrome(string2)); 
